7eb75a28a16527a61e7971def3f7165a6d3d65a6
Daniel Diamond <danieldiamond1@gmail.com>
2019-06-20 18:10:38 -0400

    modify unittest

--- auditai/tests/test_misc.py
+++ auditai/tests/test_misc.py
@@ -1,27 +1,18 @@
 import unittest
-import numpy as np
 
-from auditai.misc import bias_test_check, compare_groups
+from auditai.misc import compare_groups
 
 
 class TestMisc(unittest.TestCase):
     def setUp(self):
-        self.labels = [0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0]
-        self.results = [0.16540467, 0.09153486, 0.70881489, 0.82012178, 0.54649991,
-                        0.16622152, 0.69941364, 0.31745991, 0.72577634, 0.23298507,
-                        0.71889626, 0.30512787, 0.87571838, 0.01221256, 0.76493222,
-                        0.7943308 , 0.12108529, 0.41547848, 0.25132058, 0.33009967]
-
-    def test_bias_test_check(self):
-        category = ['Gender']
-        test_thresh = 0.50
-        bias_test_check(self.labels, self.results, category=category,
-                        test_thresh=test_thresh)
-        bias_test_check(self.labels, self.results, category=None,
-                        test_thresh=None)
+        self.labels = [0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1]
+        self.results = [0.16540, 0.09153, 0.70881, 0.82012,
+                        0.16622, 0.69941, 0.31745, 0.72577,
+                        0.71889, 0.30512, 0.87571, 0.01221,
+                        0.79433, 0.12108, 0.41547, 0.25132]
 
     def test_compare_groups(self):
-        for i in range(1,10):
+        for i in range(1, 10):
             thresh = i/10
             statistics = [j.get(thresh) for j in
                           compare_groups(self.labels, self.results, low=thresh,
